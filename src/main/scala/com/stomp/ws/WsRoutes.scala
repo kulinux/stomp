850587f671612ed3a2cdde4d20b10cdfaec1f8b2
Paco Benitez <paco.benitez.lazaro@gmail.com>
2019-03-10 22:40:22 +0100

    WebSocket

--- src/main/scala/com/stomp/ws/WsRoutes.scala
+++ src/main/scala/com/stomp/ws/WsRoutes.scala
@@ -1,7 +1,8 @@
 package com.stomp.ws
 
-import akka.actor.{ Actor, ActorSystem }
-import akka.http.scaladsl.model.ws.{ BinaryMessage, Message, TextMessage }
+import akka.actor.{ ActorSystem }
+import akka.http.scaladsl.model.{ HttpMethods, HttpRequest, HttpResponse, Uri }
+import akka.http.scaladsl.model.ws.{ BinaryMessage, Message, TextMessage, UpgradeToWebSocket }
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
@@ -20,12 +21,24 @@ trait WsRoutes {
       bm.dataStream.runWith(Sink.ignore)
       Nil
     }
+  }
 
+  val requestHandler: HttpRequest => HttpResponse = {
+    case req @ HttpRequest(HttpMethods.GET, Uri.Path("/ws"), _, _, _) =>
+      req.header[UpgradeToWebSocket] match {
+        case Some(upgrade) => upgrade.handleMessages(ws)
+        case None => HttpResponse(400, entity = "Not a valid websocket request!")
+      }
+    case r: HttpRequest =>
+      implicit val mat = ActorMaterializer()
+      r.discardEntityBytes() // important to drain incoming HTTP Entity stream
+      HttpResponse(404, entity = "Unknown resource!")
   }
 
   lazy val wsRoutes: Route =
     path("ws") {
-      handleWebSocketMessages(ws)
+      //handleWebSocketMessages(ws)
+      handleWith(requestHandler)
     }
 
 }
