84255bffff41f7638254bb73f50dca59b82abd67
Paco Benitez <paco.benitez.lazaro@gmail.com>
2019-03-23 07:32:10 +0100

    Marshall

--- src/main/scala/com/stomp/ws/parser/StompMessage.scala
+++ src/main/scala/com/stomp/ws/parser/StompMessage.scala
@@ -4,19 +4,50 @@ import akka.stream.scaladsl.Flow
 
 case class StompMessage(
   command: String,
-  header: Seq[(String, String)],
+  header: Map[String, String],
   body: String)
 
 object StompMessage {
-  def parse(str: String) = {
-    StompMessage("", Seq(), "")
 
+  val NULL : Char = 0
+  def unmarshallImpl(str: String): StompMessage = {
+    val lines = str.split("\n")
+    if(lines.size < 1) throw new RuntimeException("StompMessage unmarshallImpl error")
+    val command = lines.head
+    val headers = lines.tail
+      .takeWhile(_.isEmpty == false)
+      .map( _.split(":") )
+      .map( header => header.head -> header.tail.head)
+      .toMap
+
+    val body = lines
+      .dropWhile(_.isEmpty == false)
+      .init
+      .tail.mkString("\n")
+
+    StompMessage(command, headers, body)
   }
+
+  def marshallImpl(message: StompMessage) = {
+    val res = message.command + "\n" +
+    message.header
+      .map( hd => hd._1 + ":" + hd._2)
+      .mkString("\n") +
+    "\n" +
+    "\n" +
+    message.body +
+    "\n" +
+    NULL
+    res
+  }
+
+
+
   val unmarshall: Flow[String, StompMessage, _] =
-    Flow[String].map(parse(_))
+    Flow[String].map(unmarshallImpl(_))
 
   val process: Flow[StompMessage, StompMessage, _] =
-    Flow[StompMessage].map(sm => StompMessage("", Seq(), ""))
+    Flow[StompMessage].map(sm => StompMessage("", Map(), ""))
 
   val marshall: Flow[StompMessage, String, _] = Flow[StompMessage].map(sm => "Vuelta")
 
